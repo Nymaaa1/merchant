62155d5f4fee8fde3e516925e23d3e0d9796e390
Nyamsuren Nyamsuren <nyamsuren.ot@mobicom.mn>
2024-09-02 15:49:35 +0800

    i

--- src/components/dashboard/featureInfo.tsx
+++ src/components/dashboard/featureInfo.tsx
@@ -62,16 +62,18 @@ const FeaturedInfo = () => {
                   <span>
                     {show
                       ? '***.***'
-                      : partnerBalance?.balanceList[0].balance.toString().replace(thousands, ',') ??
-                      '0.00'}
+                      : partnerBalance?.balanceList.length > 0
+                        ? partnerBalance.balanceList[cardIndex].balance.toString().replace(thousands, ',')
+                        : '0.00'}
                     ₮
                   </span>
+
                 </div>
                 <div className="mp-wallet-id">
                   <h6>{t('mp-account')}</h6>
-                  <span>{partnerBalance?.balanceList[cardIndex].accountNo}</span>
+                  <span>{partnerBalance?.balanceList.length > 0 ? partnerBalance?.balanceList[cardIndex].accountNo : ""}</span>
                   <CopyToClipboard
-                    text={partnerBalance?.balanceList[cardIndex].accountNo ?? ""}
+                    text={partnerBalance?.balanceList.length > 0 ? partnerBalance?.balanceList[cardIndex].accountNo : ""}
                     onCopy={() =>
                       alert(
                         !copied ? 'Дансны дугаар амжилттай хуулагдлаа' : null
@@ -86,100 +88,67 @@ const FeaturedInfo = () => {
               </div>
             </div>
           </div>
-          <Col>
-            <Row className="my-3 pb-2">
-              <Col >
-                <Button
-                  variant={transactionType === 0 ? "primary" : "outline-primary"}
-                  onClick={() => setTransactionType(0)}
-                  className="d-flex justify-content-center align-items-center"
-                  style={{
-                    width: "140px",
-                    height: "48px",
-                    fontSize: '12px',
-                    padding: '8px 4px',
-                    borderRadius: '8px',
-                  }}
-                >
+          <Col >
+            <div className="card-buttons">
+              <div className="buttons-inner">
+                <Button variant={transactionType === 0 ? "primary" : "outline-primary"} onClick={() => setTransactionType(0)}>
                   Гүйлгээний хуулга
                 </Button>
-              </Col>
-              <Col >
-                <Button
-                  variant={transactionType === 1 ? "primary" : "outline-primary"}
-                  onClick={() => setTransactionType(1)}
-                  className="d-flex justify-content-center align-items-center"
-                  style={{
-                    width: "120px",
-                    height: "48px",
-                    fontSize: '12px',
-                    padding: '8px 4px',
-                    borderRadius: '8px',
-                  }}
-                >
+                <Button variant={transactionType === 1 ? "primary" : "outline-primary"} onClick={() => setTransactionType(1)}>
                   Шилжүүлэх
                 </Button>
-              </Col>
-              <Col>
-                <Button
-                  variant={transactionType === 2 ? "primary" : "outline-primary"}
-                  // className="p-2 d-flex justify-content-center align-items-center"
-                  onClick={() => setTransactionType(2)}
-                  style={{
-                    width: '48px',
-                    height: '48px',
-                    fontSize: '12px',
-                    borderRadius: '8px',
-                  }}
-                >
+                <Button variant={transactionType === 2 ? "primary" : "outline-primary"} onClick={() => setTransactionType(2)} className="d-flex justify-content-center align-items-center">
                   <img src={transactionType === 2 ? "/svg/settings-active.svg" : "/svg/settings.svg"} alt="Toggle password visibility" />
                 </Button>
-              </Col>
-            </Row>
+              </div>
+            </div>
             <h5 className='mb-2'>Бусад данс</h5>
             <Row>
               {partnerBalance?.balanceList.map((account, index) => (
-                <Col key={index} md={12} className="mb-3">
-                  <Card className="p-3" style={{ backgroundColor: '#8A94B8', color: '#fff', height: "86px", }}>
-                    <div className="mp-wallet">
-                      <div className="contents">
-                        <div className="content-inner">
-                          <div className="mp-current-wallet">
-                            <div className="content-item">
-                              <h6>{t('balance')}</h6>
-                              <div className={show ? 'hide' : 'show'}>
-                                <img onClick={toggle} src="/account-icon-on.svg" />
+                cardIndex !== index ?
+                  <Col key={index} md={12} className="mb-3" onClick={() => { setCardIndex(index); }}>
+                    <Card className="p-3" style={{ backgroundColor: '#8089AC', color: '#fff', height: "86px", }}>
+                      <div className="mp-wallet">
+                        <div className="contents">
+                          <div className="content-inner">
+                            <div className="mp-current-wallet">
+                              <div className="content-item">
+                                <h6>{t('balance')}</h6>
+                                <div className={show ? 'hide' : 'show'}>
+                                  <img onClick={toggle} src="/account-icon-on.svg" />
+                                </div>
                               </div>
+                              <span>
+                                {show
+                                  ? '***.***'
+                                  : account.balance.toString().replace(thousands, ',') ??
+                                  '0.00'}
+                                ₮
+                              </span>
                             </div>
-                            <span>
-                              {show
-                                ? '***.***'
-                                : account.balance.toString().replace(thousands, ',') ??
-                                '0.00'}
-                              ₮
-                            </span>
-                          </div>
-                          <div className="mp-wallet-id">
-                            <h6>{t('mp-account')}</h6>
-                            <span>{account?.accountNo}</span>
-                            <CopyToClipboard
-                              text={account?.accountNo}
-                              onCopy={() =>
-                                alert(
-                                  !copied ? 'Дансны дугаар амжилттай хуулагдлаа' : null
-                                )
-                              }
-                            >
-                              <div className="icon">
-                                <img src="/dbicon-copy.svg" />
-                              </div>
-                            </CopyToClipboard>
+                            <div className="mp-wallet-id">
+                              <h6>{t('mp-account')}</h6>
+                              <span>{account?.accountNo}</span>
+                              <CopyToClipboard
+                                text={account?.accountNo}
+                                onCopy={() =>
+                                  alert(
+                                    !copied ? 'Дансны дугаар амжилттай хуулагдлаа' : null
+                                  )
+                                }
+                              >
+                                <div className="icon">
+                                  <img src="/dbicon-copy.svg" />
+                                </div>
+                              </CopyToClipboard>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </Card>
-                </Col>
+                    </Card>
+                  </Col> :
+                  <></>
+
               ))}
             </Row>
           </Col>
